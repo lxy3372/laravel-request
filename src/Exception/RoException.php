34a778311c6d3d48dfbad868ab9f65db9ff7a020
ricky <lxy3372@gmail.com>
2017-06-22 18:09:46 +0800

    add file

---
+++ src/Exception/RoException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangsir
+ * Date: 16-10-28
+ * Time: 下午2:13
+ */
+
+namespace Riky\Exceptions;
+
+use Exception;
+
+/**
+ * 业务自定义异常类
+ *
+ * Class BaseException
+ *
+ * @package App\Exceptions
+ */
+class RoException extends Exception
+{
+
+}
